952918d0ea0ad2b9a2b597a955be5fb1791712c9
GeorgeDimCeid <up1057605@upnet.gr>
2021-10-23 18:11:00 +0300

    erorr

--- components/reactTutorial/FourthReactTutorial/tutorialAppTest1.js
+++ components/reactTutorial/FourthReactTutorial/tutorialAppTest1.js
@@ -2,7 +2,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
 
 
-import Home from './components/Home';
+import Home from './Home';
 
 test('renders learn react link', async () => {
   render(<Home message="ewnewje" />);
