715a1e3d8911e247c50f183f1e84f407340fcdd3
GeorgeDimCeid <up1057605@upnet.gr>
2021-10-24 16:19:44 +0300

    debug

--- pages/vueTutorial/start.js
+++ pages/vueTutorial/start.js
@@ -92,7 +92,7 @@ export default function Start({ completed }) {
   const eventHandler = (event) => {
     console.log(event);
     console.log(event.path[0].className);
-
+    console.log(event.key)
     if (event.path[0].className.includes('cm-content')) {
       if ((event.which > 46 && event.which < 91) || (event.which > 95 && event.which < 112) || (event.which > 183 && event.which < 230) || (event.which > 151 && event.which < 165)) {
         totalCharsWritten++;
